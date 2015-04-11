a1414092a5b6fcb46e8bdf794acc483fec93ec69
GuyR <guyr@wix.com>
2015-04-11 19:58:54 +0300

    position scroll handle according to current scroll

--- src/main/customScroll/customScroll.js
+++ src/main/customScroll/customScroll.js
@@ -19,7 +19,20 @@ define(['react', 'lodash', './customScroll.rt'], function (React, _, template) {
             var contentWrapper = this.refs.innerContainer.getDOMNode();
             this.scrollbarWidth = contentWrapper.offsetWidth - contentWrapper.clientWidth;
             this.contentHeight = contentWrapper.scrollHeight;
+            this.wrapperHeight = contentWrapper.clientHeight;
             this.scrollHandleHeight = contentWrapper.clientHeight * contentWrapper.clientHeight / contentWrapper.scrollHeight;
+        },
+        getScrollHandleStyle: function () {
+            var handlePosition = this.state.scrollPos + this.state.scrollPos * this.wrapperHeight / this.contentHeight;
+            return {
+                height: this.scrollHandleHeight,
+                top: handlePosition
+            };
+        },
+        onScroll: function (e) {
+            this.setState({
+                scrollPos: e.currentTarget.scrollTop
+            });
         },
         render: template
     });
