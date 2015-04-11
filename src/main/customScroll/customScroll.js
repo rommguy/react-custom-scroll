ac7d43865997c6c4b3acad348f68d4320a661f14
GuyR <guyr@wix.com>
2015-04-11 17:05:49 +0300

    create custom scroll component

---
+++ src/main/customScroll/customScroll.js
@@ -0,0 +1,8 @@
+define(['react', 'lodash', './customScroll.rt'], function (React, _, template) {
+    'use strict';
+
+    return React.createClass({
+        displayName: 'customScroll',
+        render: template
+    });
+});
