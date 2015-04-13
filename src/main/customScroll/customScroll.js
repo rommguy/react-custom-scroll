5faa77f6f4bd472fffb1759749ed13970090c7e6
GuyR <guyr@wix.com>
2015-04-13 04:34:13 +0300

    update scroll on click

--- src/main/customScroll/customScroll.js
+++ src/main/customScroll/customScroll.js
@@ -1,5 +1,23 @@
-define(['react', './customScroll.rt'], function (React, template) {
+define(['react', 'lodash', './customScroll.rt'], function (React, _, template) {
     'use strict';
+
+
+    function ensureWithinLimits(value, min, max) {
+        min = _.isUndefined(min) ? value : min;
+        max = _.isUndefined(max) ? value : max;
+        if (min > max) {
+            throw 'min limit is greater than max limit';
+        }
+
+        if (value < min) {
+            return min;
+        }
+        if (value > max) {
+            return max;
+        }
+
+        return value;
+    }
 
     return React.createClass({
         displayName: 'customScroll',
@@ -14,6 +32,8 @@ define(['react', './customScroll.rt'], function (React, template) {
             this.forceUpdate();
         },
         componentWillUpdate: function () {
+            var domNode = this.getDOMNode();
+            var boundingRect = domNode.getBoundingClientRect();
             var contentWrapper = this.refs.innerContainer.getDOMNode();
             var contentHeight = contentWrapper.scrollHeight;
             this.scrollbarYWidth = contentWrapper.offsetWidth - contentWrapper.clientWidth;
@@ -21,12 +41,33 @@ define(['react', './customScroll.rt'], function (React, template) {
             this.visibleHeight = contentWrapper.clientHeight;
             this.scrollRatio = contentHeight ? this.visibleHeight / contentHeight : 1;
             this.hasScroll = this.scrollRatio < 1;
+
+            this.position = {
+                top: boundingRect.top + window.scrollY,
+                left: boundingRect.left + window.scrollX
+            };
         },
-        getScrollHandleStyle: function (){
+        updateScrollPosition: function (scrollValue) {
+            var contentWrapper = this.refs.innerContainer.getDOMNode();
+            contentWrapper.scrollTop = scrollValue;
+            this.setState({
+                scrollPos: scrollValue
+            });
+        },
+        onCustomScrollClick: function (e) {
+            var relativeClickPosition = {
+                x: e.pageX - this.position.left,
+                y: e.pageY - this.position.top
+            };
+            // if click is below handle - jump 1 * handle Height down
+            // else - upward same distance
+            this.updateScrollPosition((relativeClickPosition.y - this.scrollHandleHeight / 2) / this.scrollRatio);
+        },
+        getScrollHandleStyle: function () {
             var handlePosition = this.state.scrollPos * this.scrollRatio;
-            var scrollHandleHeight = this.visibleHeight * this.scrollRatio;
+            this.scrollHandleHeight = this.visibleHeight * this.scrollRatio;
             return {
-                height: scrollHandleHeight,
+                height: this.scrollHandleHeight,
                 top: handlePosition
             };
         },
@@ -35,7 +76,7 @@ define(['react', './customScroll.rt'], function (React, template) {
                 scrollPos: e.currentTarget.scrollTop
             });
         },
-        blockOuterScroll: function(e){
+        blockOuterScroll: function (e) {
             var contentNode = e.currentTarget;
             var totalHeight = e.currentTarget.scrollHeight;
             var maxScroll = totalHeight - e.currentTarget.offsetHeight;
