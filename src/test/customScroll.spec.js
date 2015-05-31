502089f3a86d73c0b40abbacc1cb002edc5f4cb1
GuyR <guyr@wix.com>
2015-05-31 21:44:50 +0300

    delete old tests

--- src/test/customScroll.spec.js
+++ src/test/customScroll.spec.js
@@ -2,54 +2,6 @@ define(['react', 'customScroll/customScroll'], function (React, customScrollClas
     'use strict';
 
     describe('custom scroll', function () {
-        var TestUtils = React.addons.TestUtils;
-        var scrollbarWidth;
 
-        function getCustomScroll(props) {
-            return TestUtils.renderIntoDocument(React.createElement(customScrollClass, props));
-        }
-
-        beforeEach(function () {
-            scrollbarWidth = 17;
-        });
-
-        describe('initialization', function () {
-            it('should have scrollbar width with value 0', function () {
-                var customScroll = getCustomScroll({});
-
-                expect(customScroll.scrollbarWidth).toEqual(0);
-            });
-        });
-
-        describe('', function () {
-            var customScroll, contentWidth, contentHeight, clientHeight;
-
-            beforeEach(function () {
-                customScroll = getCustomScroll({});
-                contentWidth = 200;
-                contentHeight = 1000;
-                clientHeight = 250;
-                spyOn(customScroll.refs.innerContainer, 'getDOMNode').and.returnValue({
-                    offsetWidth: scrollbarWidth + contentWidth,
-                    clientWidth: contentWidth,
-                    scrollHeight: contentHeight,
-                    clientHeight: clientHeight
-                });
-            });
-
-            it('should update scrollbarWidth and state', function () {
-                customScroll.forceUpdate();
-
-                expect(customScroll.scrollbarWidth).toEqual(scrollbarWidth);
-            });
-
-            it('should calculate scrollbar handle height', function () {
-                customScroll.forceUpdate();
-
-                var expectedHeight = clientHeight * clientHeight / contentHeight;
-
-                expect(customScroll.scrollHandleHeight).toEqual(expectedHeight);
-            });
-        });
     });
 });
