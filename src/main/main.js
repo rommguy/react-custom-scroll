3962d47522849c8f68ad4ffe69384be0dfa20e9b
GuyR <guyr@wix.com>
2015-06-29 17:07:53 +0300

    trying to fix dependencies

--- src/main/main.js
+++ src/main/main.js
@@ -2,13 +2,13 @@ require.config({
     baseUrl:'src/main',
     paths: {
         'lodash': 'https://cdnjs.cloudflare.com/ajax/libs/lodash.js/3.6.0/lodash.min',
-        react: 'https://cdnjs.cloudflare.com/ajax/libs/react/0.13.3/react-with-addons.min',
-        jquery : '//code.jquery.com/jquery-2.1.4.min'
+        'react': 'https://cdnjs.cloudflare.com/ajax/libs/react/0.13.3/react-with-addons.min',
+        'jquery' : '//code.jquery.com/jquery-2.1.4.min'
     },
     shim: {
-        lodash: { exports: '_' },
-        react: {exports: 'React'},
-        jquery: { exports: '$' }
+        'lodash': { exports: '_' },
+        'react': {exports: 'React'},
+        'jquery': { exports: '$' }
     },
     map: {
         '*': {
