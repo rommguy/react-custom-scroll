d532dce27f3d1a7821c069393c349fa7f4800b47
Guy Romm <rommguy@gmail.com>
2020-02-10 09:47:43 +0200

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -2,7 +2,7 @@ const path = require('path')
 
 module.exports = config => {
   config.set({
-    browsers: ['Chrome'],
+    browsers: ['ChromeHeadless'],
     frameworks: ['jasmine'],
     singleRun: false,
     reporters: ['dots'], //report results in this format
