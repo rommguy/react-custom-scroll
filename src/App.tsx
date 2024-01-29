7d1e99ac4d34f6868865cfaabf024512f882d61f
rommguy <rommguy@gmail.com>
2024-01-29 17:38:43 -0500

    implement examples

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,44 @@ import { DemoComp } from "./example/demoComp.tsx";
 export const App = () => {
   return (
     <div>
-      <DemoComp />
+      <div>
+        When designing a scrollbar, we don't want to take control on the
+        scrolling mechanism. react-custom-scroll uses the existing native
+        scrolling mechanism, while only drawing the customized design. The
+        results is a smooth and natural scrolling experience. Another advantage
+        of react-custom-scroll is that the scrollbar hovers above the layout, so
+        it doesn't take up any space. This enables the same results regardless
+        of browser or operating system.
+      </div>
+      <div className="example-wrapper">
+        <DemoComp demoType="compare-with-native" />
+      </div>
+      <div id="with-crazy-design">
+        <div>There are no limits on your design.</div>
+        <div>
+          <DemoComp demoType="crazy-designer" />
+        </div>
+      </div>
+      <div id="with-dynamic-content">
+        <div>
+          When your content is dynamic, you can use the keepAtBottom prop, to
+          make sure that new content doesn't change the scroll position,
+          supporting the user experience.
+          <br />
+          In this example, scrolling the wrapping element is disabled by passing
+          false to the prop allowOuterScroll
+        </div>
+        <div>
+          <DemoComp demoType={"dynamic-content"} />
+        </div>
+      </div>
+      <div id="with-flex">
+        <DemoComp demoType="flex" />
+        <div>
+          Custom scroll supports flexible layouts. You can use it on elements
+          styled with flex, by passing the "flex" prop to CustomScroll
+        </div>
+      </div>
     </div>
   );
 };
--- src/example/demoComp.tsx
+++ src/example/demoComp.tsx
@@ -1,50 +1,129 @@
 import { CustomScroll } from "../customScroll.tsx";
 import { demoText } from "./demoText.ts";
+import { times, map } from "lodash/fp";
 import "./demoComp.css";
+import { useState } from "react";
 
-export const DemoComp = () => {
-  return (
-    <div className="example-wrapper">
-      <div key="native-example" className="container native-scroll">
-        <label className="side-title">Native Scroll</label>
+interface DemoCompProps {
+  demoType:
+    | "compare-with-native"
+    | "crazy-designer"
+    | "flex"
+    | "dynamic-content";
+}
 
-        <div className="panel">
-          <div className="panel-header">
-            <label className="panel-title">This is boring</label>
-          </div>
-          <div className="panel-content-native panel-content">
-            <div className="content-fill">{demoText.text}</div>
-          </div>
-        </div>
-      </div>
-      <div key="cool-example" className="container custom-scroll-example">
-        <label className="side-title">Custom Scroll</label>
+export const DemoComp = ({ demoType }: DemoCompProps) => {
+  const [dynamicContentCounter, setDynamicContentCounter] = useState<number>(4);
+  const addContent = () => {
+    setDynamicContentCounter((prev) => prev + 1);
+  };
+  const removeContent = () => {
+    setDynamicContentCounter((prev) => Math.max(prev - 1, 4));
+  };
+  switch (demoType) {
+    case "compare-with-native":
+      return (
+        <>
+          <div key="native-example" className="container native-scroll">
+            <label className="side-title">Native Scroll</label>
 
-        <div className="panel">
-          <div className="panel-header">
-            <label className="panel-title">Cool Scrollbar!</label>
-          </div>
-          <CustomScroll allowOuterScroll={true}>
-            <div className="panel-content-custom panel-content">
-              <div className="content-fill">{demoText.text}</div>
+            <div className="panel">
+              <div className="panel-header">
+                <label className="panel-title">This is boring</label>
+              </div>
+              <div className="panel-content-native panel-content">
+                <div className="content-fill">{demoText.text}</div>
+              </div>
             </div>
-          </CustomScroll>
-        </div>
-      </div>
-      <div key="crazy-example" className="container custom-scroll-example">
-        <label className="side-title">Crazy Designer</label>
-
-        <div className="panel crazy-scroll">
-          <div className="panel-header">
-            <label className="panel-title">Who designed this???</label>
           </div>
-          <CustomScroll allowOuterScroll={true}>
-            <div className="panel-content-custom panel-content">
-              <div className="content-fill">{demoText.text}</div>
+          <div key="cool-example" className="container custom-scroll-example">
+            <label className="side-title">Custom Scroll</label>
+
+            <div className="panel">
+              <div className="panel-header">
+                <label className="panel-title">Cool Scrollbar!</label>
+              </div>
+              <CustomScroll allowOuterScroll={true}>
+                <div className="panel-content-custom panel-content">
+                  <div className="content-fill">{demoText.text}</div>
+                </div>
+              </CustomScroll>
             </div>
-          </CustomScroll>
+          </div>
+        </>
+      );
+    case "crazy-designer":
+      return (
+        <div key="crazy-example" className="container custom-scroll-example">
+          <div className="panel crazy-scroll">
+            <div className="panel-header">
+              <label className="panel-title">Who designed this???</label>
+            </div>
+            <CustomScroll allowOuterScroll={true}>
+              <div className="panel-content-custom panel-content">
+                <div className="content-fill">{demoText.text}</div>
+              </div>
+            </CustomScroll>
+          </div>
         </div>
-      </div>
-    </div>
-  );
+      );
+    case "flex":
+      return (
+        <div key="flex-example" className="container example-flex-wrapper">
+          <div className="panel flex-scroll">
+            <div className="panel-header">
+              <label className="panel-title">Flexbox!!!</label>
+            </div>
+            <CustomScroll allowOuterScroll={true} flex="1">
+              <div className="panel-content-custom panel-content">
+                <div className="content-fill">{demoText.text}</div>
+              </div>
+            </CustomScroll>
+          </div>
+        </div>
+      );
+    case "dynamic-content":
+      return (
+        <>
+          <div className="example-dynamic-wrapper">
+            <div className="panel dynamic-scroll">
+              <div className="panel-header">
+                <label className="panel-title">DYNAMIC CONTENT!!!</label>
+              </div>
+              <CustomScroll allowOuterScroll={false} keepAtBottom={true}>
+                <div className="panel-content-custom panel-content">
+                  <div className="content-fill">
+                    {map(
+                      (content: string) => (
+                        <div className="dynamic-content" key={content}>
+                          {content}
+                        </div>
+                      ),
+                      times(
+                        (index) => `Content #${index}`,
+                        dynamicContentCounter,
+                      ),
+                    )}
+                  </div>
+                </div>
+              </CustomScroll>
+            </div>
+            <button
+              className="dynamic-content-button"
+              key="addContent"
+              onClick={addContent}
+            >
+              Add Content
+            </button>
+            <button
+              className="dynamic-content-button"
+              key="removeContent"
+              onClick={removeContent}
+            >
+              Remove Content
+            </button>
+          </div>
+        </>
+      );
+  }
 };
