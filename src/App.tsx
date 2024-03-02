083b93b51b3e2166edd162220c73e00b808bc264
rommguy <rommguy@gmail.com>
2024-03-02 12:59:05 +0200

    fix docs

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,22 @@
 import "./App.css";
 import { DemoComp } from "./example/demoComp.tsx";
-import { CustomScroll } from "./customScroll.tsx";
 
 export const App = () => {
   return (
-    <CustomScroll>
-      <div data-testid="app-root" className="app-root">
-        <div className="demo-title">React-Custom-Scroll Demo page</div>
-        <div className="demo-subtitle">
-          react-custom-scroll lets you design unique scrollbars without
-          compromising on performance. It preserves the browser's native
-          scrolling mechanism, ensuring a smooth, familiar user experience. Its
-          hover design means no content is obscured, offering a consistent look
-          across browsers and operating systems.
-        </div>
-        <DemoComp demoType="compare-with-native" descriptionSide="left" />
-        <DemoComp demoType="crazy-designer" descriptionSide="right" />
-        <DemoComp demoType="dynamic-content" descriptionSide="left" />
-        <DemoComp demoType="allow-outer-scroll" descriptionSide="right" />
-        <DemoComp demoType="flex" descriptionSide="left" />
+    <div data-testid="app-root" className="app-root">
+      <div className="demo-title">React-Custom-Scroll Demo page</div>
+      <div className="demo-subtitle">
+        react-custom-scroll lets you design unique scrollbars without
+        compromising on performance. It preserves the browser's native scrolling
+        mechanism, ensuring a smooth, familiar user experience. Its hover design
+        means no content is obscured, offering a consistent look across browsers
+        and operating systems.
       </div>
-    </CustomScroll>
+      <DemoComp demoType="compare-with-native" descriptionSide="left" />
+      <DemoComp demoType="crazy-designer" descriptionSide="right" />
+      <DemoComp demoType="dynamic-content" descriptionSide="left" />
+      <DemoComp demoType="allow-outer-scroll" descriptionSide="right" />
+      <DemoComp demoType="flex" descriptionSide="left" />
+    </div>
   );
 };
